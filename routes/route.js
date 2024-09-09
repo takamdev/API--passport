cbf825cf1f0520ce8f178cd5f63446a737a5f39f
Loic <panguslariss@gmail.com>
2024-09-09 17:22:16 +0100

    modif

--- routes/route.js
+++ routes/route.js
@@ -4,11 +4,11 @@ const route = express.Router()
 
 
 
-route.get("/api/identitydocs",getAll)
-route.post("/api/identitydocs",postPasseport)
-route.delete("/api/identitydocs/:id",deletePasseport)
-route.put("/api/identitydocs/:id",editPasseport)
-route.get("/api/identitydocs/:id",getById)
+route.get("/public/api/identitydocs",getAll)
+route.post("/public/api/identitydocs",postPasseport)
+route.delete("/public/api/identitydocs/:id",deletePasseport)
+route.put("/public/api/identitydocs/:id",editPasseport)
+route.get("/public/api/identitydocs/:id",getById)
 route.get("/",(req,res)=>{
   res.send("welcome in the passport API,use /public/api/identitydocs for get all passport registraition")
 })
